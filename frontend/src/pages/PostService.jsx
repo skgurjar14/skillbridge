729c5845cca0ae1a74c0c4e55b11e11cac919bcc
skgurjar14 <sheshkaran@gmail.com>
2026-04-15 21:25:34 +0530

    fix jobcard api import

--- frontend/src/pages/PostService.jsx
+++ frontend/src/pages/PostService.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { API } from "../config/Api";
+import { API } from "../config/api.js";
 import "../styles/postservice.css";
 
 function PostService() {
